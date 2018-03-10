03f2a6ea16c861a0aa44000cf6b1332a1ccafab4
Jennifer Cuenca <>
2018-03-09 23:02:05 -0500

    add Firebase link to index.html | added Firebase intialization code to game.js

---
+++ assets/js/game.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDbXZ_Suszm88gVV8UCv9DgbJTyNG-Ng_U",
+    authDomain: "multiplayer-rps-game-4052d.firebaseapp.com",
+    databaseURL: "https://multiplayer-rps-game-4052d.firebaseio.com",
+    projectId: "multiplayer-rps-game-4052d",
+    storageBucket: "",
+    messagingSenderId: "52108720569"
+  };
+  firebase.initializeApp(config);
